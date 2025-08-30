8ea96f4a19b6e2f3041302c156ad575064ecec24
Arianna Method <theariannamethod@gmail.com>
2025-08-30 05:11:34 +0300

    Add simple embedding storage and retrieval

--- pro_rag.py
+++ pro_rag.py
@@ -1,17 +1,24 @@
 from typing import List
+
+import numpy as np
+
 from pro_metrics import tokenize, lowercase
 import pro_memory
+import pro_rag_embedding
 
 
 async def retrieve(query_words: List[str], limit: int = 5) -> List[str]:
-    """Retrieve relevant messages from memory based on word overlap."""
-    messages, _ = await pro_memory.fetch_recent(50)
+    """Retrieve relevant messages combining word overlap and embedding similarity."""
+    messages = await pro_memory.fetch_recent_messages(50)
     qset = set(lowercase(query_words))
+    query_emb = await pro_rag_embedding.embed_sentence(' '.join(query_words))
     scored = []
-    for msg in messages:
+    for msg, emb in messages:
         words = lowercase(tokenize(msg))
-        score = len(qset.intersection(words))
-        if score:
+        word_score = len(qset.intersection(words))
+        cos_sim = float(np.dot(query_emb, emb))
+        score = word_score + cos_sim
+        if score > 0:
             scored.append((score, msg))
     scored.sort(reverse=True)
     return [m for _, m in scored[:limit]]

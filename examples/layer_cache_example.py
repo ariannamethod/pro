7557b14b054a3cca54dea364736db0b2754245ef
Arianna Method <theariannamethod@gmail.com>
2025-09-01 23:42:40 +0300

    Delete examples directory

--- examples/layer_cache_example.py
+++
@@ -1,9 +0,0 @@
-"""Example demonstrating layer construction and caching."""
-
-from memory import cache_layers
-
-macros = {"dense": {"in": 2, "out": 2}}
-
-if __name__ == "__main__":
-    layers = cache_layers(macros)
-    print("Constructed layers:", layers)

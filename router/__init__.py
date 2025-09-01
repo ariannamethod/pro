d6b2bbf286963a34c763cb297a105eb5639c33d8
Arianna Method <theariannamethod@gmail.com>
2025-09-01 23:38:32 +0300

    Delete router directory

--- router/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Routing strategies used by the engine."""
-
-from .policy import PatchRoutingPolicy, ResonantRouter
-from .subgraph import select_context_subgraph
-
-__all__ = ["PatchRoutingPolicy", "ResonantRouter", "select_context_subgraph"]
